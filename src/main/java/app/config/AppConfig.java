9fbf0dbb3f3ab50af87ef4625d80e6a91f11f4b0
Cotharsis <valeev-p@mail.ru>
2020-10-23 17:20:55 +0300

    Task 2.1.3

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,16 @@
 package app.config;
 
+import app.model.Timer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
-}
+
+
+   }
